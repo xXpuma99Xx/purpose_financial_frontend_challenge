27fb1546759abba163e1daee2c0202790bacf55a
Lemuel <lemuelhelonmarquezrosas@gmail.com>
2024-09-12 13:45:57 -0600

    Base proyecto

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- <nav>
+      <router-link to="/">Home</router-link>
+    </nav> -->
+
+    <router-view />
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "../components/HelloWorld.vue";
+</script>
+
+<style scoped></style>
